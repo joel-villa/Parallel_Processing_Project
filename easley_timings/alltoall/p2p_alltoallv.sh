a619405b81fbdb14c0f3f1e2d45b9e92af0c065c
Joel Villarreal <jvillarreal3@easley.alliance.unm.edu>
2025-11-12 13:11:52 -0700

    working point to point (I think v stands for variable length)

---
+++ easley_timings/alltoall/p2p_alltoallv.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name=CS542_p2p_alltoallv
+#SBATCH --partition=debug
+#SBATCH --nodes=2
+#SBATCH --ntasks-per-node=64
+#SBATCH --time=00:45:00
+#SBATCH --output=p2p_alltoallv.out
+#SBATCH --error=p2p_alltoallv.err
+
+module load openmpi/4.1.7-cuda-6zaq
+
+srun --nodes=2  --ntasks-per-node=64 --partition=debug ../../locality_aware/build/benchmarks/./p2p_alltoallv
